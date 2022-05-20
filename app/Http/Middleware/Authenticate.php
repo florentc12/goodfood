c1d8fc2dafbf66e7c3e3df14ea58a9749dfacf76
florentc12 <cochet.cf88@gmail.com>
2022-05-20 16:05:23 +0200

    connexion admin

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,10 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (!$request->expectsJson()) {
+            return route('admin.login');
+            //return route('/');
+            //return view('welcome');
         }
     }
 }
